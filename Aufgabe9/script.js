f08e2883f292090d0577035ba09195b1e469bed6
Grawarr <92149997+Grawarr@users.noreply.github.com>
2021-12-15 01:24:24 +0100

    Task 9 done?
    Bitte Enter Taste spammen. Sie werden sehen an sich funktioniert alles, nur sobald ich Text eingebe und dann enter drücke löscht sich die Liste irgendwie. Den Fehler finde ich leider nicht. Aber wie gesagr sonst geht alles gut.

--- Aufgabe9/script.js
+++ Aufgabe9/script.js
@@ -1,10 +1,46 @@
-//Read everything first
+//I have no idea why the notes disappear almost immediately after hitting enter key... if you just press enter without typing text everything works as intended; Buttons included. Since the text does show up for a split second I am guessing I coded in some function that deletes it automatically in sort of a loop by accident. No idea tho. I hope this suffices for now. Last time I forgot to link this script in the HTML so no wonder I got frustrated.
+//read everything first
 window.addEventListener("load", function () {
-    //Variablendeklaration
-    var taskAmount = 0;
-    function taskCounter() {
-        document.querySelector("#taskAmount").innerHTML = String(taskAmount);
+    //variables
+    var toDo = document.querySelector("ul");
+    var toDoInput = document.querySelector("#taskDefinition");
+    var index = 0;
+    //function taskAmount
+    function taskAmount() {
+        document.querySelector("#taskAmount").innerHTML = String(index);
     }
+    //function taskAdd
+    function taskAdd() {
+        index++;
+        taskAmount();
+        //adds list with input
+        var toDoList = document.createElement("li");
+        toDoList.classList.add("todotext");
+        toDoList.innerHTML = toDoInput.value;
+        //adds empty circle icon
+        var circle = document.createElement("button");
+        circle.classList.add("circle");
+        circle.setAttribute("class", "far fa-circle");
+        toDoList.appendChild(circle);
+        //adds minus circle icon
+        var taskRemove = document.createElement("button");
+        taskRemove.classList.add("circleMinus");
+        taskRemove.setAttribute("class", "fas fa-minus-circle");
+        toDoList.appendChild(taskRemove);
+        //appends list into HTML
+        toDo.appendChild(toDoList);
+        console.log(toDoList);
+        //reverts value
+        toDoInput.value = "";
+        //click detection and mark as done toggle
+        taskRemove.addEventListener("click", function () { toDo.removeChild(toDoList); index--; taskAmount(); });
+        circle.addEventListener("click", function () { circle.classList.toggle("fa-dot-circle"); });
+    }
+    //adds function to enter key
+    document.addEventListener("keydown", function (event) {
+        if (event.keyCode == 13) {
+            taskAdd();
+        }
+    });
 });
-//Ich brauche mehr Zeit sorry... Typescript ist noch nicht so ganz bei mir angekommen.
 //# sourceMappingURL=script.js.map
--- Aufgabe9/script.ts
+++ Aufgabe9/script.ts
@@ -1,12 +1,46 @@
-//Read everything first
+//I have no idea why the notes disappear almost immediately after hitting enter key... if you just press enter without typing text everything works as intended; Buttons included. Since the text does show up for a split second I am guessing I coded in some function that deletes it automatically in sort of a loop by accident. No idea tho. I hope this suffices for now. Last time I forgot to link this script in the HTML so no wonder I got frustrated.
+
+//read everything first
 window.addEventListener("load", function () {
-    //Variablendeklaration
-    
-    let taskAmount:number = 0
-
-    function taskCounter() {
-        document.querySelector("#taskAmount").innerHTML = String(taskAmount);
+    //variables
+    const toDo = document.querySelector("ul");
+    const toDoInput = (<HTMLInputElement>document.querySelector("#taskDefinition"));
+    let index = 0;
+    //function taskAmount
+    function taskAmount() {
+        document.querySelector("#taskAmount").innerHTML = String(index);
     }
+    //function taskAdd
+    function taskAdd() {
+        index++;
+        taskAmount();
+        //adds list with input
+        const toDoList = document.createElement("li");
+        toDoList.classList.add("todotext");
+        toDoList.innerHTML = toDoInput.value;
+        //adds empty circle icon
+        let circle = document.createElement("button");
+        circle.classList.add("circle");
+        circle.setAttribute("class", "far fa-circle");
+        toDoList.appendChild(circle);
+        //adds minus circle icon
+        const taskRemove = document.createElement("button");
+        taskRemove.classList.add("circleMinus");
+        taskRemove.setAttribute("class", "fas fa-minus-circle");
+        toDoList.appendChild(taskRemove);
+        //appends list into HTML
+        toDo.appendChild(toDoList);
+        console.log(toDoList);
+        //reverts value
+        toDoInput.value = "";
+        //click detection and mark as done toggle
+        taskRemove.addEventListener("click", function () { toDo.removeChild(toDoList); index--; taskAmount(); });
+        circle.addEventListener("click", function () {circle.classList.toggle("fa-dot-circle")} );
+    }
+    //adds function to enter key
+    document.addEventListener("keydown", function (event) {
+        if (event.keyCode == 13) {
+            taskAdd();
+        }
+    });
 });
-
-//Ich brauche mehr Zeit sorry... Typescript ist noch nicht so ganz bei mir angekommen.
